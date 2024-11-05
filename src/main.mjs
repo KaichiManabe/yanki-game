ac2452dd7d924535f7e417063a59d3ee129ff3f8
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-11-05 19:58:26 +0900

    基礎的なステージを作成

---
+++ src/main.mjs
@@ -0,0 +1,109 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("wall", "assets/platform.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+
+function create() {
+  this.add.image(400, 300, "sky");
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 32, "wall").setScale(2).refreshBody();
+  platforms.create(400, 568, "wall").setScale(2).refreshBody();
+
+  player = this.physics.add.sprite(100, 450, "dude");
+
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "up",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "down",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.physics.add.collider(player, platforms);
+}
+
+function update() {
+  cursors = this.input.keyboard.createCursorKeys();
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play("right", true);
+  } else if (cursors.up.isDown) {
+    player.setVelocityY(-160);
+
+    player.anims.play("up", true);
+  } else if (cursors.down.isDown) {
+    player.setVelocityY(160);
+    
+    player.anims.play("down", true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play("turn");
+  }
+}
