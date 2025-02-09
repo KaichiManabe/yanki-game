5a1374f30c2bc6367d01cebc9ba9e4eb57fbb909
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-02-09 15:59:21 +0900

    敵情報を統一

--- src/stages/stage3.mjs
+++ src/stages/stage3.mjs
@@ -1,14 +1,56 @@
 import { GameSceneBase } from "./gameSceneBase.mjs";
 
 const enemyInfo = [
-  { x: 300, y: 150, velocity: { x: 150, y: 150 }, rotate: true, circle: false },
-  { x: 400, y: 100, velocity: { x: 150, y: 150 }, rotate: false, circle: true },
   {
+    name: 2,
+    x: 300,
+    y: 150,
+    w: 40,
+    h: 30,
+    rotate: true,
+    circle: false,
+    hitSize: {
+      height: 3,
+      width: 1,
+    },
+    velocity: {
+      x: 150,
+      y: 150,
+    },
+  },
+  {
+    name: 3,
+    x: 400,
+    y: 100,
+    w: 30,
+    h: 30,
+    rotate: false,
+    circle: true,
+    hitSize: {
+      height: 1.5,
+      width: 1,
+    },
+    velocity: {
+      x: 150,
+      y: 150,
+    },
+  },
+  {
+    name: 4,
     x: 500,
     y: 250,
-    velocity: { x: 150, y: 150 },
+    w: 30,
+    h: 30,
     rotate: false,
     circle: false,
+    hitSize: {
+      height: 1.5,
+      width: 4,
+    },
+    velocity: {
+      x: 150,
+      y: 150,
+    },
   },
 ];
 
