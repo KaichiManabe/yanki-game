0eeda3fbaf24c16485add740c1b98ee7eb881698
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-02-09 18:36:50 +0900

    change again

--- src/stages/gameSceneBase.mjs
+++ src/stages/gameSceneBase.mjs
@@ -10,11 +10,11 @@ export class GameSceneBase extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("sky", "/sky.png");
-    this.load.image("wall", "/platform.png");
-    this.load.image("star", "/star.png");
-    this.load.image("setting", "/setting.png");
-    this.load.spritesheet("dude", "/dude.png", {
+    this.load.image("sky", "/assets/sky.png");
+    this.load.image("wall", "/assets/platform.png");
+    this.load.image("star", "/assets/star.png");
+    this.load.image("setting", "/assets/setting.png");
+    this.load.spritesheet("dude", "/assets/dude.png", {
       frameWidth: 32,
       frameHeight: 48,
     });
